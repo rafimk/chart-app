7fd52a7c1dace4bdcd72fdaaf39be679e507e5fd
rafimk <rafi.genius.cs@gmail.com>
2023-09-17 21:12:25 +0400

    updated and changed to base64 bit string

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import io
 import os  # Import the os module
+import base64
 
 app = Flask(__name__)
 
@@ -68,8 +69,8 @@ def generate_chart():
         return Response(img_data.getvalue(), content_type='image/png')
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
-    
+
+
 @app.route('/ikigai', methods=['POST'])
 def draw_ikigai():
     try:
@@ -90,27 +91,33 @@ def draw_ikigai():
         r = 1.25
         offset = 0.6  # Adjusted for central intersection
         centers = [(0, offset), (-offset, 0), (0, -offset), (offset, 0)]
-        
+
         # Colors for the circles
         colors = ['#FF9999', '#66B2FF', '#99FF99', '#FFCC99']
-        
+
         # Create circles
-        circles = [plt.Circle(center, r, color=color, alpha=0.4) for center, color in zip(centers, colors)]
+        circles = [plt.Circle(center, r, color=color, alpha=0.4)
+                   for center, color in zip(centers, colors)]
         for circle in circles:
             ax.add_patch(circle)
 
         # Adjusted positions for each circle's label to the outer edge
-        label_positions = [(0, offset + r - 0.1), (-offset - r + 0.1, 0), (0, -offset - r + 0.1), (offset + r - 0.1, 0)]
+        label_positions = [(0, offset + r - 0.1), (-offset - r + 0.1, 0),
+                           (0, -offset - r + 0.1), (offset + r - 0.1, 0)]
         for position, label in zip(label_positions, labels):
-            ax.text(position[0], position[1], label, ha='center', va='center', fontsize=9, fontweight='bold')
+            ax.text(position[0], position[1], label, ha='center',
+                    va='center', fontsize=9, fontweight='bold')
 
         # Set text for the overlaps
-        overlap_positions = [(-offset, offset), (-offset, -offset), (offset, -offset), (offset, offset)]
+        overlap_positions = [
+            (-offset, offset), (-offset, -offset), (offset, -offset), (offset, offset)]
         for position, label in zip(overlap_positions, overlap_labels):
-            ax.text(position[0], position[1], label, ha='center', va='center', fontsize=9, fontweight='bold', backgroundcolor='white', zorder=5)
+            ax.text(position[0], position[1], label, ha='center', va='center',
+                    fontsize=9, fontweight='bold', backgroundcolor='white', zorder=5)
 
         # Highlight central IKIGAI text
-        ax.text(0, 0, title, ha='center', va='center', fontsize=20, fontweight='bold', color='#555555', zorder=5, backgroundcolor='white')
+        ax.text(0, 0, title, ha='center', va='center', fontsize=20,
+                fontweight='bold', color='#555555', zorder=5, backgroundcolor='white')
 
         ax.set_xlim(-2.5, 2.5)
         ax.set_ylim(-2.5, 2.5)
@@ -129,8 +136,143 @@ def draw_ikigai():
         # Return the image binary data as a Flask response
         return Response(img_data.getvalue(), content_type='image/png')
     except Exception as e:
-        return jsonify({"error": str(e)}), 500    
-    
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/chartb', methods=['POST'])
+def generate_chartb():
+    try:
+        # Get data from the request JSON
+        data = request.json
+
+        # Ensure that the request contains 'labels', 'sizes', and 'colors' keys
+        if 'data' not in data or 'categories' not in data or 'title' not in data:
+            return jsonify({"error": "Invalid data format"}), 400
+
+        data_points = data['data']
+        areas = data['categories']
+        title = data['title']
+
+        N = len(areas)
+        theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
+        radii = data_points
+        width = np.pi / 4 * np.ones(N)
+
+        fig, ax = plt.subplots(figsize=(6, 6), subplot_kw={
+                               'projection': 'polar'})
+        bars = ax.bar(theta, radii, width=width, bottom=0.0,
+                      align='center', edgecolor='gray', linewidth=0.5)
+
+        # A clearer color palette - can be customized
+        colors = ["#FF9999", "#66B2FF", "#99FF99", "#FFCC99",
+                  "#FFD700", "#C71585", "#20B2AA", "#FF4500"]
+        for bar, color in zip(bars, colors):
+            bar.set_facecolor(color)
+            bar.set_alpha(0.7)
+
+        # Remove gridlines and outer circle (spine)
+        ax.yaxis.grid(False)
+        ax.xaxis.grid(False)
+        ax.spines["polar"].set_visible(False)
+
+        # Remove ytick labels and set xtick labels with padding and custom font properties
+        ax.set_yticklabels([])
+        ax.set_xticks(theta)
+        ax.set_xticklabels(areas, fontdict={
+                           'fontsize': 10, 'fontweight': 'bold', 'color': '#555555'})
+
+        # Set title with custom font properties
+        ax.set_title(title, va='bottom', fontdict={
+                     'fontsize': 14, 'fontweight': 'bold'})
+
+        # Add padding to the image
+        plt.tight_layout()
+
+        # Save the chart as bytes in memory
+        img_data = io.BytesIO()
+        plt.savefig(img_data, format='png')
+        img_data.seek(0)
+        plt.close()
+
+       # Encode the image data as base64 and return it as a string
+        base64_img = base64.b64encode(img_data.read()).decode('utf-8')
+
+        # Return the base64-encoded image as a string in the response
+        return base64_img
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/ikigaib', methods=['POST'])
+def draw_ikigaib():
+    try:
+        # Get data from the request JSON
+        data = request.json
+
+        # Ensure that the request contains 'labels', 'sizes', and 'colors' keys
+        if 'labels' not in data or 'overlap' not in data or 'title' not in data:
+            return jsonify({"error": "Invalid data format"}), 400
+
+        labels = data['labels']
+        overlap_labels = data['overlap']
+        title = data['title']
+
+        fig, ax = plt.subplots(figsize=(10, 10))
+
+        # Define circle radius and centers for a diagonal orientation
+        r = 1.25
+        offset = 0.6  # Adjusted for central intersection
+        centers = [(0, offset), (-offset, 0), (0, -offset), (offset, 0)]
+
+        # Colors for the circles
+        colors = ['#FF9999', '#66B2FF', '#99FF99', '#FFCC99']
+
+        # Create circles
+        circles = [plt.Circle(center, r, color=color, alpha=0.4)
+                   for center, color in zip(centers, colors)]
+        for circle in circles:
+            ax.add_patch(circle)
+
+        # Adjusted positions for each circle's label to the outer edge
+        label_positions = [(0, offset + r - 0.1), (-offset - r + 0.1, 0),
+                           (0, -offset - r + 0.1), (offset + r - 0.1, 0)]
+        for position, label in zip(label_positions, labels):
+            ax.text(position[0], position[1], label, ha='center',
+                    va='center', fontsize=9, fontweight='bold')
+
+        # Set text for the overlaps
+        overlap_positions = [
+            (-offset, offset), (-offset, -offset), (offset, -offset), (offset, offset)]
+        for position, label in zip(overlap_positions, overlap_labels):
+            ax.text(position[0], position[1], label, ha='center', va='center',
+                    fontsize=9, fontweight='bold', backgroundcolor='white', zorder=5)
+
+        # Highlight central IKIGAI text
+        ax.text(0, 0, title, ha='center', va='center', fontsize=20,
+                fontweight='bold', color='#555555', zorder=5, backgroundcolor='white')
+
+        ax.set_xlim(-2.5, 2.5)
+        ax.set_ylim(-2.5, 2.5)
+        ax.set_aspect('equal', 'box')
+        ax.axis('off')
+
+        # Add padding to the image
+        plt.tight_layout()
+
+        # Save the chart as bytes in memory
+        img_data = io.BytesIO()
+        plt.savefig(img_data, format='png')
+        img_data.seek(0)
+        plt.close()
+
+       # Encode the image data as base64 and return it as a string
+        base64_img = base64.b64encode(img_data.read()).decode('utf-8')
+
+        # Return the base64-encoded image as a string in the response
+        return base64_img
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=api_port, debug=False,  threaded=False)
