47bb141d02f3992ac7ef0f1a0a53cb7f1b88ff3a
rafimk <rafi.genius.cs@gmail.com>
2023-09-18 22:10:44 +0400

    updated with url name

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, Response, request, jsonify
+import os
+import io
 import matplotlib.pyplot as plt
 import numpy as np
-import io
-import os  # Import the os module
-import base64
+from flask import Flask, request, Response, jsonify, send_from_directory
 
 app = Flask(__name__)
+
+# Directory to store generated chart images
+CHARTS_DIR = 'charts'
+
+# Ensure the charts directory exists
+os.makedirs(CHARTS_DIR, exist_ok=True)
 
 api_port = int(os.environ.get('API_PORT', 80))
 
@@ -272,6 +277,71 @@ def draw_ikigaib():
         return base64_img
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+@app.route('/charturl', methods=['POST'])
+def generate_chart_url():
+    try:
+        # Get data from the request JSON
+        data = request.json
+
+        # Ensure that the request contains 'data', 'categories', and 'title' keys
+        if 'data' not in data or 'categories' not in data or 'title' not in data:
+            return jsonify({"error": "Invalid data format"}), 400
+
+        data_points = data['data']
+        areas = data['categories']
+        title = data['title']
+
+        N = len(areas)
+        theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
+        radii = data_points
+        width = np.pi / 4 * np.ones(N)
+
+        fig, ax = plt.subplots(figsize=(6, 6), subplot_kw={
+                               'projection': 'polar'})
+        bars = ax.bar(theta, radii, width=width, bottom=0.0,
+                      align='center', edgecolor='gray', linewidth=0.5)
+
+        colors = ["#FF9999", "#66B2FF", "#99FF99", "#FFCC99",
+                  "#FFD700", "#C71585", "#20B2AA", "#FF4500"]
+        for bar, color in zip(bars, colors):
+            bar.set_facecolor(color)
+            bar.set_alpha(0.7)
+
+        ax.yaxis.grid(False)
+        ax.xaxis.grid(False)
+        ax.spines["polar"].set_visible(False)
+        ax.set_yticklabels([])
+        ax.set_xticks(theta)
+        ax.set_xticklabels(areas, fontdict={
+                           'fontsize': 10, 'fontweight': 'bold', 'color': '#555555'})
+        ax.set_title(title, va='bottom', fontdict={
+                     'fontsize': 14, 'fontweight': 'bold'})
+
+        plt.tight_layout()
+
+        # Generate a unique filename for the chart image
+        chart_filename = f"{title.replace(' ', '_')}.png"
+        chart_path = os.path.join(CHARTS_DIR, chart_filename)
+
+        # Save the chart as a PNG image
+        plt.savefig(chart_path, format='png')
+        plt.close()
+
+        # Construct the URL for the saved chart image
+        chart_url = f"/{CHARTS_DIR}/{chart_filename}"
+
+        # Return the URL in the response
+        return jsonify({"chart_url": chart_url})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    # Serve static files from the 'charts' directory
+    return send_from_directory(CHARTS_DIR, filename)
 
 
 if __name__ == '__main__':
